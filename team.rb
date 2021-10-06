aacea936630e4f96a1230bc3a738d29567eebf4a
yuuta.ootuka <yuuta.19950912@gmail.com>
2021-10-06 19:00:41 +0000

    kadai_team_ruby

---
+++ team.rb
@@ -0,0 +1,32 @@
+class Team
+ attr_accessor :name, :win, :lose, :draw
+    
+ def initialize(name, win, lose, draw)
+   self.name = name
+   self.win = win
+   self.lose = lose
+   self.draw = draw
+ end 
+ 
+ def calc_win_rate
+   win.to_f/(win+lose)
+ end 
+ 
+ def show_team_result()
+   puts "#{self.name}の2020年の成績は#{self.win}勝 #{self.lose}敗 #{self.draw}分 勝率は#{self.calc_win_rate}です。"
+ end 
+end 
+
+Giants = Team.new("Giants", 67, 45, 8)
+Tigers = Team.new("Tigers", 60, 53, 7)
+Dragons = Team.new("Dragons", 60, 55, 5)
+BayStars = Team.new("BayStars", 56, 58, 6)
+Carp = Team.new("Carp", 52, 56, 12)
+Swallows = Team.new("Swallows", 41, 69, 10)
+
+Giants.show_team_result
+Tigers.show_team_result
+Dragons.show_team_result
+BayStars.show_team_result
+Carp.show_team_result
+Swallows.show_team_result
